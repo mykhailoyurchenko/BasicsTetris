71ce591a781e3883559721713b6f7769ca2b182a
STALJERST <staljer246@gmail.com>
2025-05-19 13:33:59 +0300

    git commit "start"

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by STALJ on 19.05.2025.
+//
+#include <iostream>
+#include "main.h"
+
+int main (){
+std::cout << "Hello, World!" << std::endl;
+}
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by STALJ on 19.05.2025.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
