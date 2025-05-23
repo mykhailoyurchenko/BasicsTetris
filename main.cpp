495b7cb9498d473decaccb8c43583892f749d790
mykhailoyurchenko <gast101112@gmail.com>
2025-05-23 18:17:35 +0300

    Code rafactoring

--- main.cpp
+++ main.cpp
@@ -6,107 +6,73 @@
 using namespace sf;
 using namespace std;
 
-vector<RectangleShape> rectangles;
-
-void createButton(RectangleShape button, float mouseX,float mouseY) {
-	float lBorderB1 = button.getPosition().x;
-	float dBorderB1 = button.getPosition().y;
-	float rBorderB1 = lBorderB1 + button.getSize().x;
-	float uBorderB1 = dBorderB1 + button.getSize().y;
-	//перевірка на взаємодію з кноакою
-	if ((mouseX >= lBorderB1) && (mouseX <= rBorderB1) && (mouseY >= dBorderB1) && (mouseY <= uBorderB1)) {
-		button.setFillColor(Color(20, 90, 0));
-		if (Mouse::isButtonPressed(Mouse::Button::Left)) {
-			RectangleShape tetrisRect(Vector2f(500, 900));
-			tetrisRect.setFillColor(sf::Color::Black);
-			tetrisRect.setPosition(Vector2f(700, 50));
-			rectangles.push_back(tetrisRect);
-			button.setFillColor(Color::Yellow);
-
-		}
-		else {
-			button.setFillColor(Color(0, 0, 0, 12));
-		}
-	}
-
-}
-
-
 
 int main()
 {
-
-
+	vector<RectangleShape> rectangles;
 
 	RenderWindow window(VideoMode({ 1920, 1080 }), "Tetris", Style::Default,State::Fullscreen);
 	window.setFramerateLimit(60);
 
+	RectangleShape game(Vector2f(400, 550));
+	RectangleShape button1(Vector2f(200, 50));
+	RectangleShape button2(Vector2f(200, 50));
 
-	RectangleShape rect;
-	rect.setFillColor(Color(0, 0, 0, 12));
-	rect.setPosition(Vector2f(750, 500));
-	rect.setSize(Vector2f(400, 550));
-
-
-	//квадрат/кнопка
-	RectangleShape button1;
-	button1.setFillColor(Color(0, 100, 100, 128));
-	button1.setPosition(Vector2f(850, 500));
-	button1.setSize(Vector2f(200, 50));
-
-
-	RectangleShape button2;
-	button2.setFillColor(Color(0, 0, 0, 70));
-	button2.setPosition(Vector2f(1450, 500));
-	button2.setSize(Vector2f(200, 50));
-
-	//налаштування фону
-	Texture backgroundTexture;
-	if (!backgroundTexture.loadFromFile("src/texture/background.jpg")) {
-		return -1; // Помилка завантаження
-	}
-
+	Texture backgroundTexture("src/texture/background.jpg");
 	Sprite background(backgroundTexture);
-	background.setTexture(backgroundTexture);
-
-	//налаштування шрифта
 	Font font;
+
+	if (!backgroundTexture.loadFromFile("src/texture/background.jpg")) return -1; // Помилка завантаження
 	if (!font.openFromFile("src/fonts/Roboto_Condensed-Black.ttf")) return -1;
 
+	game.setFillColor(Color(0, 0, 0, 12));
+	game.setPosition(Vector2f(750, 500));
+	//квадрат/кнопка
+	button1.setFillColor(Color(0, 100, 100, 128));
+	button1.setPosition(Vector2f(850, 500));
+
+	button2.setFillColor(Color(0, 0, 0, 70));
+	button2.setPosition(Vector2f(1450, 500));
+	
+
 	//налаштування тексту
-	Text textButton1(font);
-	textButton1.setString("play");
-	textButton1.setCharacterSize(50);
+	Text textButton1(font,"Play",50);
+	Text goBack(font, "Go back", 50);
+	Text scoreText(font, "0", 40);
+
 	textButton1.setFillColor(Color::White);
 	textButton1.setPosition(Vector2f(900, 490));
 
-	Text goBack(font);
-	goBack.setString("Go back");
-	goBack.setCharacterSize(50);
 	goBack.setFillColor(Color::White);
 	goBack.setPosition(Vector2f(1475, 490));
 
-	int score = 0;
-	Text scoreText(font);
-	scoreText.setCharacterSize(40);
 	scoreText.setFillColor(Color::White);
 	scoreText.setPosition(Vector2f(50, 50));
 
+
+	int scoreCounrter = 0;
 	while (window.isOpen()) {
 
 		//відстежувіння мишки
-		float mouseX = Mouse::getPosition(window).x;
-		float mouseY = Mouse::getPosition(window).y;
+		//float mouseX = Mouse::getPosition(window).x;
+		//float mouseY = Mouse::getPosition(window).y;
 
-		//кординати кінця і початку кнопки
-		createButton(button1, mouseX, mouseY);
+		Vector2f mousePos = Vector2f(Mouse::getPosition(window));
 
-		float lBorderB2 = button2.getPosition().x;
-		float dBorderB2 = button2.getPosition().y;
-		float rBorderB2 = lBorderB2 + button2.getSize().x;
-		float uBorderB2 = dBorderB2 + button2.getSize().y;
+		if (button1.getGlobalBounds().contains(mousePos)) {
+			button1.setFillColor(Color(20, 90, 0));
+			if (Mouse::isButtonPressed(Mouse::Button::Left)) {
+				RectangleShape tetrisRect(Vector2f(500, 900));
+				tetrisRect.setFillColor(sf::Color::Black);
+				tetrisRect.setPosition(Vector2f(700, 50));
+				rectangles.push_back(tetrisRect);
+				button1.setFillColor(Color::Yellow);
 
-		auto mousePos = Vector2f(Mouse::getPosition(window));
+			}
+			else {
+				button1.setFillColor(Color(0, 0, 0, 12));
+			}
+		}
 
 		if (button2.getGlobalBounds().contains(mousePos)) {
 			button2.setFillColor(Color(20, 90, 0));
@@ -128,9 +94,9 @@ int main()
 				window.close();
 		}
 
-		ostringstream ss;
-		ss << "Score: " << score;
-		scoreText.setString(ss.str());
+		ostringstream scoreLeadOut;
+		scoreLeadOut << "Score: " << scoreCounrter;
+		scoreText.setString(scoreLeadOut.str());
 
 		//рендер
 		window.clear();
@@ -143,8 +109,9 @@ int main()
 
 
 
-		for (const auto& tetrisRect : rectangles)
+		for (const auto& tetrisRect : rectangles) {
 			window.draw(tetrisRect);
+		}
 		window.display();
 
 	}
