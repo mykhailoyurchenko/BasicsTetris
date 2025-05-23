00c66ed592bf6dcb5377a4b987441fac94765da8
STALJERST <staljer246@gmail.com>
2025-05-23 11:05:37 +0300

    Refactor main.cpp to replace circles with rectangles and update button interactions; add score display

--- main.cpp
+++ main.cpp
@@ -3,20 +3,12 @@
 using namespace sf;
 using namespace std;
 
-
-
-
-
-
-
-
 int main()
 {
 
-
     RenderWindow window(VideoMode({1920, 1080}), "Tetris");
 
-    std::vector<sf::CircleShape> circles;
+    std::vector<sf::RectangleShape> rectagles;
 
     RectangleShape rect;
     rect.setFillColor(Color(0, 0, 0, 12));
@@ -32,7 +24,7 @@ int main()
 
         RectangleShape rectButton2;
         rectButton2.setFillColor(Color(0, 0, 0, 12));
-        rectButton2.setPosition(Vector2f(850, 900));
+        rectButton2.setPosition(Vector2f(1450, 500));
         rectButton2.setSize(Vector2f(200, 50));
 
 
@@ -52,10 +44,17 @@ int main()
 
     //налаштування тексту
     Text textButton1(font);
-    textButton1.setString("text");
+    textButton1.setString("play");
     textButton1.setCharacterSize(50);
     textButton1.setFillColor(Color::White);
-    textButton1.setPosition(Vector2f(850, 500));
+    textButton1.setPosition(Vector2f(900, 490));
+
+    int score = 0;
+    sf::Text scoreText(font);
+    scoreText.setCharacterSize(40);
+    scoreText.setFillColor(sf::Color::White);
+    scoreText.setPosition(Vector2f(50, 50));
+
 
     //перевірка чи рендериця фон
     if (!backgroundTexture.loadFromFile("texture/background.jpg")) {
@@ -78,20 +77,38 @@ int main()
         float x2 = x1 + rectButton1.getSize().x;
         float y2 = y1 + rectButton1.getSize().y;
 
+        float x11 = rectButton2.getPosition().x;
+        float y11 = rectButton2.getPosition().y;
+        float x22 = x11 + rectButton2.getSize().x;
+        float y22 = y11 + rectButton2.getSize().y;
+
         //перевірка на взаємодію з кноакою
-        if ((mouseX >= x1)and(mouseX <= x2) and (mouseY >= y1)  and (mouseY <= y2)) {
+        if ((mouseX >= x1) && (mouseX <= x2) && (mouseY >= y1)  && (mouseY <= y2)) {
             rectButton1.setFillColor(Color(20, 90, 0));
             if (Mouse::isButtonPressed(Mouse::Button::Left)) {
-                sf::CircleShape circle(30.f);
-                circle.setFillColor(sf::Color::Green);
-                circle.setPosition(Vector2f(400, 300));
-                circles.push_back(circle);
+                RectangleShape tetrisRect(Vector2f(500, 900));
+                tetrisRect.setFillColor(sf::Color::Black);
+                tetrisRect.setPosition(Vector2f(700, 50));
+                rectagles.push_back(tetrisRect);
                 rectButton1.setFillColor(Color::Yellow);
+
             }
         } else {
             rectButton1.setFillColor(Color((0, 0, 0, 12)));
         }
 
+        if ((mouseX >= x11) && (mouseX <= x22) && (mouseY >= y11) && (mouseY <= y22)) {
+            rectButton2.setFillColor(Color(20, 90, 0));
+            if (Mouse::isButtonPressed(Mouse::Button::Left)) {
+                if (!rectagles.empty()) {
+                    rectagles.pop_back(); // Remove last rectangle
+
+                }
+                rectButton2.setFillColor(Color::Yellow);
+            }
+        }else {
+            rectButton2.setFillColor(Color((0, 0, 0, 12)));
+        }
 
         //обробка події закриття
         while (const optional event = window.pollEvent()) {
@@ -99,15 +116,19 @@ int main()
                 window.close();
         }
 
+        std::ostringstream ss;
+        ss << "Score: " << score;
+        scoreText.setString(ss.str());
+
         //рендер
         window.clear();
         window.draw(background);
         window.draw(rectButton1);
         window.draw(rectButton2);
         window.draw(textButton1);
-
-        for (const auto& circle : circles)
-            window.draw(circle);
+        window.draw(scoreText);
+        for (const auto& tetrisRect : rectagles)
+            window.draw(tetrisRect);
 
         window.display();
     }
