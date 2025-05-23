6d9e949638c76dac18e5b19b5900b5b3722f7da7
mykhailoyurchenko <gast101112@gmail.com>
2025-05-23 12:25:52 +0300

    Изменения в CMakeLists.txt и main.cpp
    Обновлены пути копируемых файлов в CMakeLists.txt для соответствия новой структуре каталогов.
    
    В main.cpp проведена рефакторинг кода:
    - Упрощены названия переменных и улучшена читаемость.
    - Изменен порядок и структура кода, инициализация окна теперь происходит в начале.
    - Упрощена логика проверки взаимодействия с кнопками.
    - Добавлены комментарии на украинском языке для лучшего понимания кода.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <Menu.h>
-
-#include "src/Menu.h"
+#include <sstream>
 
 using namespace sf;
 using namespace std;
@@ -9,131 +8,129 @@ using namespace std;
 int main()
 {
 
+	RenderWindow window(VideoMode({ 1920, 1080 }), "Tetris");
+	window.setFramerateLimit(60);
 
-    RenderWindow window(VideoMode({1920, 1080}), "Tetris");
+	vector<RectangleShape> rectangles;
 
-    std::vector<sf::RectangleShape> rectagles;
-
-    RectangleShape rect;
-    rect.setFillColor(Color(0, 0, 0, 12));
-    rect.setPosition(Vector2f(750, 500));
-    rect.setSize(Vector2f(400, 550));
-
-        //квадрат/кнопка
-        RectangleShape rectButton1;
-        rectButton1.setFillColor(Color(0, 100, 100, 128));
-        rectButton1.setPosition(Vector2f(850, 500));
-        rectButton1.setSize(Vector2f(200, 50));
+	RectangleShape rect;
+	rect.setFillColor(Color(0, 0, 0, 12));
+	rect.setPosition(Vector2f(750, 500));
+	rect.setSize(Vector2f(400, 550));
 
 
-        RectangleShape rectButton2;
-        rectButton2.setFillColor(Color(0, 0, 0, 12));
-        rectButton2.setPosition(Vector2f(1450, 500));
-        rectButton2.setSize(Vector2f(200, 50));
+	//квадрат/кнопка
+	RectangleShape button1;
+	button1.setFillColor(Color(0, 100, 100, 128));
+	button1.setPosition(Vector2f(850, 500));
+	button1.setSize(Vector2f(200, 50));
 
 
+	RectangleShape button2;
+	button2.setFillColor(Color(0, 0, 0, 12));
+	button2.setPosition(Vector2f(1450, 500));
+	button2.setSize(Vector2f(200, 50));
 
-    window.setFramerateLimit(60);
+	//налаштування фону
+	Texture backgroundTexture;
+	backgroundTexture.loadFromFile("src/texture/background.jpg");
+	Sprite background(backgroundTexture);
+	background.setTexture(backgroundTexture);
 
+	//налаштування шрифта
+	Font font;
+	if (!font.openFromFile("src/fonts/Roboto_Condensed-Black.ttf")) return 3;
 
-    //налаштування фону
-    Texture backgroundTexture;
-    backgroundTexture.loadFromFile("texture/background.jpg");
-    Sprite background(backgroundTexture);
-    background.setTexture(backgroundTexture);
+	//налаштування тексту
+	Text textButton1(font);
+	textButton1.setString("play");
+	textButton1.setCharacterSize(50);
+	textButton1.setFillColor(Color::White);
+	textButton1.setPosition(Vector2f(900, 490));
 
-    //налаштування шрифта
-    Font font;
-    if (!font.openFromFile("fonts/Roboto_Condensed-Black.ttf"))return 3;
-
-    //налаштування тексту
-    Text textButton1(font);
-    textButton1.setString("play");
-    textButton1.setCharacterSize(50);
-    textButton1.setFillColor(Color::White);
-    textButton1.setPosition(Vector2f(900, 490));
-
-    int score = 0;
-    sf::Text scoreText(font);
-    scoreText.setCharacterSize(40);
-    scoreText.setFillColor(sf::Color::White);
-    scoreText.setPosition(Vector2f(50, 50));
-
-
-    //перевірка чи рендериця фон
-    if (!backgroundTexture.loadFromFile("texture/background.jpg")) {
-        return -1; // Помилка завантаження
-    }
-
-    bool showRectangle = false;
-
-    while (window.isOpen()){
+	int score = 0;
+	sf::Text scoreText(font);
+	scoreText.setCharacterSize(40);
+	scoreText.setFillColor(sf::Color::White);
+	scoreText.setPosition(Vector2f(50, 50));
 
 
-        //відстежувіння мишки
-        float mouseX = Mouse::getPosition(window).x;
-        float mouseY = Mouse::getPosition(window).y;
+	//перевірка чи рендериця фон
+	if (!backgroundTexture.loadFromFile("src/texture/background.jpg")) {
+		return -1; // Помилка завантаження
+	}
+
+	bool showRectangle = false;
+
+	while (window.isOpen()) {
 
 
-        //кординати кінця і початку кнопки
-        float x1 = rectButton1.getPosition().x;
-        float y1 = rectButton1.getPosition().y;
-        float x2 = x1 + rectButton1.getSize().x;
-        float y2 = y1 + rectButton1.getSize().y;
+		//відстежувіння мишки
+		float mouseX = Mouse::getPosition(window).x;
+		float mouseY = Mouse::getPosition(window).y;
 
-        float x11 = rectButton2.getPosition().x;
-        float y11 = rectButton2.getPosition().y;
-        float x22 = x11 + rectButton2.getSize().x;
-        float y22 = y11 + rectButton2.getSize().y;
 
-        //перевірка на взаємодію з кноакою
-        if ((mouseX >= x1) && (mouseX <= x2) && (mouseY >= y1)  && (mouseY <= y2)) {
-            rectButton1.setFillColor(Color(20, 90, 0));
-            if (Mouse::isButtonPressed(Mouse::Button::Left)) {
-                RectangleShape tetrisRect(Vector2f(500, 900));
-                tetrisRect.setFillColor(sf::Color::Black);
-                tetrisRect.setPosition(Vector2f(700, 50));
-                rectagles.push_back(tetrisRect);
-                rectButton1.setFillColor(Color::Yellow);
+		//кординати кінця і початку кнопки
+		float x1 = button1.getPosition().x;
+		float y1 = button1.getPosition().y;
+		float x2 = x1 + button1.getSize().x;
+		float y2 = y1 + button1.getSize().y;
 
-            }
-        } else {
-            rectButton1.setFillColor(Color((0, 0, 0, 12)));
-        }
+		float x11 = button2.getPosition().x;
+		float y11 = button2.getPosition().y;
+		float x22 = x11 + button2.getSize().x;
+		float y22 = y11 + button2.getSize().y;
 
-        if ((mouseX >= x11) && (mouseX <= x22) && (mouseY >= y11) && (mouseY <= y22)) {
-            rectButton2.setFillColor(Color(20, 90, 0));
-            if (Mouse::isButtonPressed(Mouse::Button::Left)) {
-                if (!rectagles.empty()) {
-                    rectagles.pop_back(); // Remove last rectangle
+		//перевірка на взаємодію з кноакою
+		if ((mouseX >= x1) && (mouseX <= x2) && (mouseY >= y1) && (mouseY <= y2)) {
+			button1.setFillColor(Color(20, 90, 0));
+			if (Mouse::isButtonPressed(Mouse::Button::Left)) {
+				RectangleShape tetrisRect(Vector2f(500, 900));
+				tetrisRect.setFillColor(sf::Color::Black);
+				tetrisRect.setPosition(Vector2f(700, 50));
+				rectangles.push_back(tetrisRect);
+				button1.setFillColor(Color::Yellow);
 
-                }
-                rectButton2.setFillColor(Color::Yellow);
-            }
-        }else {
-            rectButton2.setFillColor(Color((0, 0, 0, 12)));
-        }
+			}
+		}
+		else {
+			button1.setFillColor(Color((0, 0, 0, 12)));
+		}
 
-        //обробка події закриття
-        while (const optional event = window.pollEvent()) {
-            if (event->is<Event::Closed>())
-                window.close();
-        }
+		if ((mouseX >= x11) && (mouseX <= x22) && (mouseY >= y11) && (mouseY <= y22)) {
+			button2.setFillColor(Color(20, 90, 0));
+			if (Mouse::isButtonPressed(Mouse::Button::Left)) {
+				if (!rectangles.empty()) {
+					rectangles.pop_back(); // Remove last rectangle
 
-        std::ostringstream ss;
-        ss << "Score: " << score;
-        scoreText.setString(ss.str());
+				}
+				button2.setFillColor(Color::Yellow);
+			}
+		}
+		else {
+			button2.setFillColor(Color(0, 0, 0, 12));
+		}
 
-        //рендер
-        window.clear();
-        window.draw(background);
-        window.draw(rectButton1);
-        window.draw(rectButton2);
-        window.draw(textButton1);
-        window.draw(scoreText);
-        for (const auto& tetrisRect : rectagles)
-            window.draw(tetrisRect);
+		//обробка події закриття
+		while (const optional event = window.pollEvent()) {
+			if (event->is<Event::Closed>())
+				window.close();
+		}
 
-        window.display();
-    }
+		ostringstream ss;
+		ss << "Score: " << score;
+		scoreText.setString(ss.str());
+
+		//рендер
+		window.clear();
+		window.draw(background);
+		window.draw(button1);
+		window.draw(button2);
+		window.draw(textButton1);
+		window.draw(scoreText);
+		for (const auto& tetrisRect : rectangles)
+			window.draw(tetrisRect);
+
+		window.display();
+	}
 }
