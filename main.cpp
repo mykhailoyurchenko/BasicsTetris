97aa52b8fda45e93b05e6fd0812d552d1b8733ae
mykhailoyurchenko <gast101112@gmail.com>
2025-05-24 23:29:28 +0300

    Улучшение управления графическими объектами(ALARM проблема з вказівникми,використовувати обережно)
    Изменена структура хранения объектов `RectangleShape` и `Text` на указатели для более эффективного управления памятью. Обновлены размеры и позиции графических элементов, добавлены новые кнопки и текстовые элементы. Реализованы проверки перед удалением объектов из векторов и добавлено очищение векторов `rectangles` и `texts` в конце цикла отрисовки для предотвращения утечек памяти.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <Menu.h>
-#include <random>
 #include <sstream>
+#include <iostream>
+
 
 using namespace sf;
 using namespace std;
@@ -9,17 +10,18 @@ using namespace std;
 
 int main()
 {
-	vector<RectangleShape> rectangles;
+	vector<RectangleShape*> rectangles;
+	vector<Text*> texts;
 
-	RenderWindow window(VideoMode({ 1920, 1080 }), "Tetris", Style::Default,State::Fullscreen);
+	RenderWindow window(VideoMode({ 1920, 1080 }), "Tetris", Style::Default, State::Fullscreen);
 	window.setFramerateLimit(60);
 	Image icon;
 	if (!icon.loadFromFile("src/texture/icon.png")) return -1; // Помилка завантаження
 	window.setIcon(icon);
 
-	RectangleShape game(Vector2f(400, 550));
-	RectangleShape button1(Vector2f(200, 50));
-	RectangleShape button2(Vector2f(200, 50));
+	RectangleShape game(Vector2f(500, 900));
+	RectangleShape playButton(Vector2f(150, 50));
+	RectangleShape backButton(Vector2f(150, 50));
 
 	Texture backgroundTexture;
 	if (!backgroundTexture.loadFromFile("src/texture/background.jpg")) return -1; // Помилка завантаження
@@ -27,37 +29,45 @@ int main()
 
 	Font tetrisFont;
 	Font font;
-	
+
 	if (!font.openFromFile("src/fonts/Roboto_Condensed-Black.ttf")) return -1;
-	if(!tetrisFont.openFromFile("src/fonts/Oi-Regular.ttf")) return -1; // Помилка завантаження
+	if (!tetrisFont.openFromFile("src/fonts/Oi-Regular.ttf")) return -1; // Помилка завантаження
 
-	game.setFillColor(Color(0, 0, 0, 12));
-	game.setPosition(Vector2f(750, 500));
+	game.setFillColor(Color::Black);
+	game.setPosition(Vector2f(700, 50));
+
 	//квадрат/кнопка
-	button1.setFillColor(Color(0, 100, 100, 128));
-	button1.setPosition(Vector2f(850, 500));
+	playButton.setFillColor(Color(0, 0, 0, 0.0));
+	playButton.setPosition(Vector2f(850, 500));
+	rectangles.push_back(&playButton);
 
-	button2.setFillColor(Color(0, 0, 0, 70));
-	button2.setPosition(Vector2f(1450, 500));
-	
+	backButton.setFillColor(Color(0, 0, 0, 70));
+	backButton.setPosition(Vector2f(1480, 500));
+	rectangles.push_back(&backButton);
 
 	//налаштування тексту
-	Text textButton1(font,"Play",50);
-	Text goBack(font, "Go back", 50);
+	Text playButtonText(font, "Play", 50);
+	Text backButtonText(font, "Go back", 50);
 	Text scoreText(font, "0", 40);
 	Text tetrisText(tetrisFont, "Tetris", 70);
 
-	textButton1.setFillColor(Color::White);
-	textButton1.setPosition(Vector2f(900, 490));
 
-	goBack.setFillColor(Color::White);
-	goBack.setPosition(Vector2f(1475, 490));
+	playButtonText.setFillColor(Color::White);
+	playButtonText.setPosition(Vector2f(900, 490));
+	texts.push_back(&playButtonText);
+
+	backButtonText.setFillColor(Color::White);
+	backButtonText.setPosition(Vector2f(1475, 490));
+	texts.push_back(&backButtonText);
 
 	scoreText.setFillColor(Color::White);
 	scoreText.setPosition(Vector2f(50, 50));
+	texts.push_back(&scoreText);
 
 	tetrisText.setFillColor(Color::White);
 	tetrisText.setPosition(Vector2f(200, 500));
+	texts.push_back(&tetrisText);
+
 
 	int scoreCounrter = 0;
 	while (window.isOpen()) {
@@ -68,32 +78,38 @@ int main()
 
 		Vector2f mousePos = Vector2f(Mouse::getPosition(window));
 
-		if (button1.getGlobalBounds().contains(mousePos)) {
-			button1.setFillColor(Color(20, 90, 0));
+		if (playButton.getGlobalBounds().contains(mousePos)) {
+			playButtonText.setFillColor(Color::Yellow);
 			if (Mouse::isButtonPressed(Mouse::Button::Left)) {
-				RectangleShape tetrisRect(Vector2f(500, 900));
-				tetrisRect.setFillColor(sf::Color::Black);
-				tetrisRect.setPosition(Vector2f(700, 50));
-				rectangles.push_back(tetrisRect);
-				button1.setFillColor(Color::Yellow);
-
-			}
-			else {
-				button1.setFillColor(Color(0, 0, 0, 12));
-			}
-		}
-
-		if (button2.getGlobalBounds().contains(mousePos)) {
-			button2.setFillColor(Color(20, 90, 0));
-			if (Mouse::isButtonPressed(Mouse::Button::Left)) {
-				if (!rectangles.empty()) {
-					rectangles.pop_back(); // Remove last rectangle
+				rectangles.push_back(&game);
+				auto i = find(texts.begin(), texts.end(), &playButtonText);
+				if (i != texts.end()) {
+					texts.erase(i);
 				}
-				button2.setFillColor(Color::Yellow);
+				auto j = find(rectangles.begin(), rectangles.end(), &playButton);
+				if (j != rectangles.end()) {
+					rectangles.erase(j);
+				}
 			}
 		}
 		else {
-			button2.setFillColor(Color(0, 20, 100, 70));
+			playButtonText.setFillColor(Color::White);
+		}
+
+		if (backButton.getGlobalBounds().contains(mousePos)) {
+			backButton.setFillColor(Color(20, 90, 0));
+			if (Mouse::isButtonPressed(Mouse::Button::Left)) {
+				auto i = find(rectangles.begin(), rectangles.end(), &game);
+				if (i != rectangles.end()) {
+					rectangles.erase(i);
+				}
+				texts.push_back(&playButtonText);
+				rectangles.push_back(&playButton);
+				backButton.setFillColor(Color::Yellow);
+			}
+		}
+		else {
+			backButton.setFillColor(Color(0, 20, 100, 70));
 		}
 
 		//обробка події закриття
@@ -109,18 +125,16 @@ int main()
 		//рендер
 		window.clear();
 		window.draw(background);
-		window.draw(button1);
-		window.draw(button2);
-		window.draw(textButton1);
-		window.draw(scoreText);
-		window.draw(goBack);
-		window.draw(tetrisText);
 
 		for (const auto& tetrisRect : rectangles) {
-			window.draw(tetrisRect);
+			window.draw(*tetrisRect);
+		}
+		for (const auto& tetrisText : texts) {
+			window.draw(*tetrisText);
 		}
 
 		window.display();
-
 	}
+	rectangles.clear();
+	texts.clear();
 }
