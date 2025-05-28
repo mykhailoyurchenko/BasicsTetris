27a3fa470791573f3d21b30290b4461b22cf9aa0
Andrey33106 <andreychernyak2@gmail.com>
2025-05-28 18:31:33 +0300

    Додав коментарі до файлу MenuState.cpp

--- MenuState.cpp
+++ MenuState.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
 
+// Конструктор меню
 MenuState::MenuState(Game& game) : GameState(game), playButton(Vector2f(150, 50)), playButtonText(font, "Play", 50) {
 	playButton.setFillColor(Color(0, 0, 0, 0.0));
 	playButton.setPosition(Vector2f(850, 500));
@@ -7,10 +8,11 @@ MenuState::MenuState(Game& game) : GameState(game), playButton(Vector2f(150, 50)
 	playButtonText.setFillColor(Color::White);
 	playButtonText.setPosition(Vector2f(900, 490));
 }
+// Обробка подій на клік
 void MenuState::eventHandler(Event& event) {
 	GameState::eventHandler(event);
 	const auto* mouseEvent = event.getIf<Event::MouseButtonPressed>();
-	if (mouseEvent && mouseEvent->button == Mouse::Button::Left) {
+	if (mouseEvent && mouseEvent->button == Mouse::Button::Left) { // Перевірка натискання миші
 
 		Vector2f mousePos = Vector2f(Mouse::getPosition(game->getWindow()));
 		if (playButton.getGlobalBounds().contains(mousePos)) {
@@ -18,16 +20,19 @@ void MenuState::eventHandler(Event& event) {
 		}
 	}
 }
+// Оновлення стану
 void MenuState::update() {
 	Vector2f mousePos = Vector2f(Mouse::getPosition(game->getWindow()));
+	// Зміна кольору тексту 
 	if (playButton.getGlobalBounds().contains(mousePos)) {
 		playButtonText.setFillColor(Color::Yellow);
 	}
 	else playButtonText.setFillColor(Color::White);
 }
+// Відображення меню
 void MenuState::draw(RenderWindow& window) {
-	GameState::draw(window);
+	GameState::draw(window); // Відмальовка
 	window.draw(playButton);
 	window.draw(playButtonText);
 	window.display();
-}
+}	
