f7f94e0186666a33824d8c94912fefd79f12f28e
Andrey33106 <andreychernyak2@gmail.com>
2025-05-29 23:36:19 +0300

    Додання необхідних візуальних рішень
    * Добавлены счет и время в состояние игры
    
    В `playState.cpp` изменен конструктор `PlayState` для добавления текстовых элементов, отображающих счет и время. Обновлены позиции интерфейсных элементов и добавлены вызовы для их отрисовки в методе `draw`.
    
    В `States.h` добавлены новые поля в класс `PlayState` и обновлены комментарии для улучшения ясности кода.
    
    * Додав найкращий результат
    
    ---------
    
    Co-authored-by: Vlad1236311 <prijmakvlad7@gmail.com>
    Co-authored-by: mykhailoyurchenko <gast101112@gmail.com>

--- playState.cpp
+++ playState.cpp
@@ -1,15 +1,39 @@
 ﻿#include "Game.h"
+
 // Констуктор класу PlayState
 PlayState::PlayState(Game& game) : GameState(game), gameField(24, 16, 25, 750, 100), gameFieldNext(Vector2f(230, 600)), backButton(Vector2f(150, 50)),
-backButtonText(font, "Go back", 50), scoreText(font, "0", 40) {
+backButtonText(font, "Go back", 50), scoreText(font, "0", 40),, scoreOutur(font, "0", 40), timeText(font, "Time:", 40), timeOutur(font, "00:00:00", 40), 
+bestScoreOutur(font, "0", 40), bestScoreText(font, "Best Score:", 40) {
+
 	backButton.setFillColor(Color(0, 0, 0, 0.0));
 	backButton.setPosition(Vector2f(1750, 50));
 
 	backButtonText.setFillColor(Color::White);
 	backButtonText.setPosition(Vector2f(1740, 40));
 
+	//Лічильник для підрахунку очков
+	scoreOutur.setFillColor(Color::White);
+	scoreOutur.setPosition(Vector2f(1617, 752));
+
+	//Текст для підрахунку очков
 	scoreText.setFillColor(Color::White);
-	scoreText.setPosition(Vector2f(50, 50));
+	scoreText.setPosition(Vector2f(1507, 750));
+
+	//Текст часу
+	timeText.setFillColor(Color::White);
+	timeText.setPosition(Vector2f(1520, 900));
+
+	//Лічильник часу
+	timeOutur.setFillColor(Color::White);
+	timeOutur.setPosition(Vector2f(1500, 960));
+
+	//Лічільник найкращого результату
+	bestScoreOutur.setFillColor(Color::White);
+	bestScoreOutur.setPosition(Vector2f(1665, 827));
+
+	// текст найкращого результату
+	bestScoreText.setFillColor(Color::White);
+	bestScoreText.setPosition(Vector2f(1472, 825));
 
 	gameFieldNext.setPosition(Vector2f(1200, 150));
 	gameFieldNext.setFillColor(Color(0, 255, 0, 150));
@@ -37,7 +61,11 @@ void PlayState::draw(RenderWindow& window) {
 	window.draw(gameField);
 	window.draw(gameFieldNext);
 	window.draw(scoreText);
-
+	window.draw(scoreOutur);
+	window.draw(timeText);
+	window.draw(timeOutur);
+	window.draw(bestScoreText);
+	window.draw(bestScoreOutur);
 
 }
 // зміна колір тексту кнопки при наведенні миші
